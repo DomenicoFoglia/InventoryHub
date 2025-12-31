e79cd5e1f97aab2760c2fa5ad704c55787d19b3e
DomenicoFoglia <foglia.dmnc@gmail.com>
2025-12-31 16:26:37 +0100

    Miglioramenti vari sulla gestione degli errori e sul frontend

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -8,12 +8,25 @@ var app = builder.Build();
 // Configurazione CORS: permette al Client di fare domande al Server
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-app.MapGet("/api/products", () =>
-{
+app.MapGet("/api/productlist", () => {
     return new[]
     {
-        new { Id = 1, Name = "Laptop", Price = 1200.50, Stock = 25 },
-        new { Id = 2, Name = "Headphones", Price = 50.00, Stock = 100 }
+        new
+        {
+            Id = 1,
+            Name = "Laptop",
+            Price = 1200.50,
+            Stock = 25,
+            Category = new { Id = 101, Name = "Electronics" } // Oggetto annidato
+        },
+        new
+        {
+            Id = 2,
+            Name = "Headphones",
+            Price = 50.00,
+            Stock = 100,
+            Category = new { Id = 102, Name = "Accessories" } // Oggetto annidato
+        }
     };
 });
 
